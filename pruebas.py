2bf3d2bfb50d95e3186722639b567a8934194d28
RouseIsela <rcordovar29@gmail.com>
2023-01-22 19:13:56 -0500

    Version 2.0

--- pruebas.py
+++ pruebas.py
@@ -2,4 +2,4 @@ from datetime import datetime
 ahora=datetime.now()
 navidad = datetime(2023,12,25)
 tiempo_falta = navidad - ahora
-print(tiempo_falta.days)
+print(f"Los dias que faltan son {tiempo_falta.days}")
